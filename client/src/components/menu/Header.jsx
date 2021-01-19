9883e113502ffddc3e5475edbe6adb2f5fb5c43c
nicoWeb31 <nico.riot@free.fr>
2021-01-19 17:39:03 +0100

    css header

--- client/src/components/menu/Header.jsx
+++ client/src/components/menu/Header.jsx
@@ -1,47 +1,56 @@
 import {
-    SettingOutlined
+    AppstoreOutlined,
+    SettingOutlined,
+    UserOutlined,
+    UserAddOutlined,
 } from "@ant-design/icons";
 import { Menu } from "antd";
-import React, { useState } from 'react';
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
+import "./header.style.scss";
 
-const { SubMenu } = Menu;
+const { SubMenu,Item } = Menu;
 
 const Header = () => {
+    const [current, setCurrent] = useState("");
 
-    const [current,setCurrent] = useState('')
- 
     const handleClick = (e) => {
-        console.log("click ", e);
-        setCurrent(e.key );
+        setCurrent(e.key);
     };
 
+    return (
+        <Menu
+            onClick={handleClick}
+            selectedKeys={[current]}
+            mode="horizontal"
+            classnames="_navbar"
+        >
+            <Item key="home" icon={<AppstoreOutlined />}>
+                <Link to="/">Home</Link>
+            </Item>
 
+            <SubMenu key="SubMenu" icon={<SettingOutlined />} title="UserName">
+                <Item key="setting:1">Option 1</Item>
+                <Item key="setting:2">Option 2</Item>
+            </SubMenu>
 
-        return (
-            <Menu
-                onClick={handleClick}
-                selectedKeys={[current]}
-                mode="horizontal"
-            >
-                <Menu.Item key="mail">
-                    Home
-                </Menu.Item>
-                
-                <SubMenu
-                    key="SubMenu"
-                    icon={<SettingOutlined />}
-                    title="Register"
+                <Item
+                    key="register"
+                    icon={<UserAddOutlined />}
+                    className="float-right"
                 >
+                    <Link to="/register">Register</Link>
+                </Item>
+                <Item
+                    key="login"
+                    icon={<UserOutlined />}
+                    className="float-right"
+                >
+                    <Link to="/login">Login</Link>
+                </Item>
 
-                        <Menu.Item key="setting:1">Option 1</Menu.Item>
-                        <Menu.Item key="setting:2">Option 2</Menu.Item>
-
-
-                </SubMenu>
-
-            </Menu>
-        );
-    
-}
+        </Menu>
+    );
+};
 
 export default Header;
