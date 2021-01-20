c4c197e10fb48bb57f3f07a3d6f92756b3d1f1f2
nicoWeb31 <nico.riot@free.fr>
2021-01-20 15:44:45 +0100

    routine redux

--- client/src/components/app/App.js
+++ client/src/components/app/App.js
@@ -2,9 +2,11 @@ import Register from "../../pages/auth/register/Register";
 import Home from "../../pages/home/Home";
 import Login from "../../pages/auth/login/Login";
 import { Switch, Route } from "react-router-dom";
-import { ToastContainer} from 'react-toastify';
-import 'react-toastify/dist/ReactToastify.css'
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
+import { Provider } from "react-redux";
+import store from "../../redux/store";
 
 import "./app.style.scss";
 import Header from "../menu/Header";
@@ -13,21 +15,23 @@ import RegisterComplete from "../../pages/auth/registerComplete/RegisterComplete
 const App = () => {
     return (
         <>
+            <Provider store={store}>
+                <Header />
 
-            <Header/>
-
-            <Switch>
-
-                <Route path="/" exact component={Home} />
-                <Route path="/login" exact component={Login} />
-                <Route path="/register" exact component={Register} />
-                <Route path="/register/complete" exact component={RegisterComplete} />
-
-
-            </Switch>
-            <ToastContainer/>
+                <Switch>
+                    <Route path="/" exact component={Home} />
+                    <Route path="/login" exact component={Login} />
+                    <Route path="/register" exact component={Register} />
+                    <Route
+                        path="/register/complete"
+                        exact
+                        component={RegisterComplete}
+                    />
+                </Switch>
+                <ToastContainer />
+            </Provider>
         </>
     );
-}
+};
 
 export default App;
