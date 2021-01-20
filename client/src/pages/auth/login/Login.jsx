7109253ba9a13168ac382c310c18f6dac9eb0fee
nicoWeb31 <nico.riot@free.fr>
2021-01-20 18:48:27 +0100

    login pages with password and email ok

--- client/src/pages/auth/login/Login.jsx
+++ client/src/pages/auth/login/Login.jsx
@@ -3,14 +3,37 @@ import { auth } from "../../../firebase";
 import { toast } from "react-toastify";
 import { Button } from "antd";
 import { MailOutlined } from "@ant-design/icons";
+import { useDispatch } from "react-redux";
+import { loginUser } from "../../../redux/actions/authAction";
 
-const Login = () => {
+const Login = ({ history }) => {
     const [email, setEmail] = useState("");
     const [password, setPassword] = useState("");
+    const [loading, setLoding] = useState(false);
+
+    const dispatch = useDispatch();
 
     const HandleSubmit = async (e) => {
         e.preventDefault();
+        setLoding(true);
         console.table(email, password);
+        try {
+            const { user } = await auth.signInWithEmailAndPassword(
+                email,
+                password
+            );
+
+            const idTokenResult = await user.getIdTokenResult();
+
+            dispatch(
+                loginUser({ email: user.email, token: idTokenResult.token })
+            );
+            toast.success("Login with success");
+            history.push("/");
+        } catch (error) {
+            toast.error(error.message);
+            setLoding(false);
+        }
     };
 
     //____________________form_________________________________
@@ -42,9 +65,9 @@ const Login = () => {
                     className="mb-3"
                     block
                     shape="round"
-                    icon={<MailOutlined/>}
+                    icon={<MailOutlined />}
                     onClick={HandleSubmit}
-                    disabled={!email || password.length < 6} 
+                    disabled={!email || password.length < 6}
                 >
                     {" "}
                     Login with Email/Password
