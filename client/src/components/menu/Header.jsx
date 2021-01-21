0d84e9f4653546da9b5260a264882e9b790ca8f1
nicoWeb31 <nico.riot@free.fr>
2021-01-21 10:38:20 +0100

    render header conditione

--- client/src/components/menu/Header.jsx
+++ client/src/components/menu/Header.jsx
@@ -8,7 +8,7 @@ import {
 import { Menu } from "antd";
 import firebase from "firebase";
 import React, { useState } from "react";
-import { useDispatch } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import { Link, useHistory } from "react-router-dom";
 import { logoutUser } from "../../redux/actions/authAction";
 import "./header.style.scss";
@@ -20,6 +20,8 @@ const Header = () => {
 
     const dispatch = useDispatch();
     const history = useHistory();
+
+    const userAuhtState = useSelector((state) => state.auth);
 
     const handleClick = (e) => {
         setCurrent(e.key);
@@ -42,24 +44,36 @@ const Header = () => {
                 <Link to="/">Home</Link>
             </Item>
 
-            <SubMenu key="SubMenu" icon={<SettingOutlined />} title="UserName">
-                <Item key="setting:1">Option 1</Item>
-                <Item key="setting:2">Option 2</Item>
-                <Item icon={<LogoutOutlined />} onClick={logout}>
-                    Logout
-                </Item>
-            </SubMenu>
-
-            <Item
-                key="register"
-                icon={<UserAddOutlined />}
-                className="float-right"
-            >
-                <Link to="/register">Register</Link>
-            </Item>
-            <Item key="login" icon={<UserOutlined />} className="float-right">
-                <Link to="/login">Login</Link>
-            </Item>
+            {!userAuhtState ? (
+                <>
+                    <Item
+                        key="login"
+                        icon={<UserOutlined />}
+                        className="float-right"
+                    >
+                        <Link to="/login">Login</Link>
+                    </Item>
+                    <Item
+                        key="register"
+                        icon={<UserAddOutlined />}
+                        className="float-right"
+                    >
+                        <Link to="/register">Register</Link>
+                    </Item>
+                </>
+            ) : (
+                <SubMenu
+                    key="SubMenu"
+                    icon={<SettingOutlined />}
+                    title="UserName"
+                >
+                    <Item key="setting:1">Option 1</Item>
+                    <Item key="setting:2">Option 2</Item>
+                    <Item icon={<LogoutOutlined />} onClick={logout}>
+                        Logout
+                    </Item>
+                </SubMenu>
+            )}
         </Menu>
     );
 };
--- client/src/pages/auth/login/Login.jsx
+++ client/src/pages/auth/login/Login.jsx
@@ -1,8 +1,8 @@
 import React, { useState } from "react";
-import { auth } from "../../../firebase";
+import { auth, googleAuthProvider } from "../../../firebase";
 import { toast } from "react-toastify";
 import { Button } from "antd";
-import { MailOutlined } from "@ant-design/icons";
+import { MailOutlined,GoogleOutlined } from "@ant-design/icons";
 import { useDispatch } from "react-redux";
 import { loginUser } from "../../../redux/actions/authAction";
 
@@ -35,6 +35,23 @@ const Login = ({ history }) => {
             setLoding(false);
         }
     };
+
+    const HandleSubmitWithGoogle = async()=>{
+        auth.signInWithPopup(googleAuthProvider)
+        .then(async (result) => {
+            const { user } = result;
+            const idTokenResult = await user.getIdTokenResult();
+
+            dispatch(
+                loginUser({ email: user.email, token: idTokenResult.token })
+            );
+            toast.success("Login with success");
+            history.push("/");
+        })
+        .catch(
+            (err)=> toast.error(err.message)
+        )
+    }
 
     //____________________form_________________________________
     const loginForm = () => {
@@ -80,8 +97,21 @@ const Login = ({ history }) => {
         <div className="container p-5">
             <div className="row">
                 <div className="col-md-6 offset-md-3">
-                    <h4>Login</h4>
+                    { !loading ? (<h4>Login</h4>) : (<h4 className="text-danger">Loding....</h4>)}
                     {loginForm()}
+
+                    <Button
+                    type="danger"
+                    block
+                    shape="round"
+                    icon={<GoogleOutlined />}
+                    onClick={HandleSubmitWithGoogle}
+                    size="large"
+
+                >
+                    {" "}
+                    Login with Google
+                </Button>
                 </div>
                 <div className="col-6"></div>
             </div>
